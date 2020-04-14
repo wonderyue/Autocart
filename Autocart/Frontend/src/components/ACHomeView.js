e3fd569990da5bf7665729351562920c94d68a81
wondery <wonderdaystudio@163.com>
2020-04-14 11:13:54 -0400

    HomePage

--- Autocart/Frontend/src/components/ACHomeView.js
+++ Autocart/Frontend/src/components/ACHomeView.js
@@ -1,19 +1,53 @@
 import React, { Component } from "react";
-import { Embed, Grid } from "semantic-ui-react";
+import { Embed, Grid, Button, Header } from "semantic-ui-react";
+import { MEDIA_URL } from "@src/constants";
+import { Link } from "react-router-dom";
 
 class ACHomeView extends Component {
   state = {};
   render() {
     return (
-      <Grid centered style={{ margin: "1em 0em 1em 0em" }}>
-        <Grid.Column style={{ width: "60em" }}>
+      <Grid
+        centered
+        style={{
+          height: "120em",
+          backgroundImage: `url(${MEDIA_URL + "bg3.jpg"})`,
+          backgroundSize: "cover",
+        }}
+      >
+        <Grid.Column textAlign="center">
           <Embed
-            style={{ width: "80em" }}
+            style={{ width: "80em", margin: "3em" }}
             id="Oq_VFGtEfig"
             source="youtube"
+            aspectRatio="21:9"
             autoplay
             defaultActive
+            hd
           />
+          <Header style={{ fontSize: "5em" }} inverted>
+            AUTOCART
+          </Header>
+          <Button
+            color="blue"
+            size="big"
+            style={{ marginTop: "5em" }}
+            as={Link}
+            to="/cars/"
+          >
+            Find your next car
+          </Button>
+          <p
+            style={{
+              position: "absolute",
+              bottom: "0%",
+              right: "0%",
+              color: "white",
+              margin: "0.2em",
+            }}
+          >
+            Photo by Dimitar Donovski on Unsplash
+          </p>
         </Grid.Column>
       </Grid>
     );
