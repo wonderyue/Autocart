484d278c19369b339760b48f219fe428513803ac
wondery <wonderdaystudio@163.com>
2020-04-15 17:04:39 -0400

    MEDIA_ROOT, STATIC_ROOT

--- Autocart/Autocart/urls.py
+++ Autocart/Autocart/urls.py
@@ -16,14 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("Frontend.urls")),
     path('', include("Backend.urls")),
+    url(r'^media/(?P<path>.*)$', serve,
+        {'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,
+        {'document_root': settings.STATIC_ROOT}),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
