2cf8b87775fa9d1d50e0a6f78478d87fad2cd12c
wondery <wonderdaystudio@163.com>
2020-05-01 10:32:41 -0400

    admin

--- Autocart/Backend/admin.py
+++ Autocart/Backend/admin.py
@@ -4,3 +4,6 @@ from . import models
 admin.site.register(models.User)
 admin.site.register(models.Car)
 admin.site.register(models.Cart)
+admin.site.register(models.Order)
+admin.site.register(models.CarImage)
+admin.site.register(models.Comment)
