cc7948fbc858267a8d23a9fc89c7e7fa2debf43e
yue <yuewenduo@gmail.com>
2020-08-04 20:43:26 -0400

    static root

--- Autocart/Autocart/settings.py
+++ Autocart/Autocart/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 REACT_APP_DIR = os.path.join(BASE_DIR, 'Frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'static', 'frontend'),
