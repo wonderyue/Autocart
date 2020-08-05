ce07eb9a3ae6819eb5ece8a1f5c94a474abdb542
yue <yuewenduo@gmail.com>
2020-08-04 21:16:23 -0400

    dir

--- Autocart/Autocart/settings.py
+++ Autocart/Autocart/settings.py
@@ -14,7 +14,6 @@ import os
 from datetime import timedelta
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -129,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 REACT_APP_DIR = os.path.join(BASE_DIR, 'Frontend')
 STATICFILES_DIRS = [
     os.path.join(REACT_APP_DIR, 'static', 'frontend'),
